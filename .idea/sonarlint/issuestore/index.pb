c96d9f94b327e01658865fefadefe239f8e0e5e2
Mitchell <msaat429@gmail.com>
2022-10-18 14:55:14 -0600

    add db functionality

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+R
+"src/main/java/DatabaseManager.java,3\a\3a95d45747abf54af39ee15fc1f54cba61d5da73
--- src/main/java/DatabaseManager.java
+++ src/main/java/DatabaseManager.java
@@ -1,12 +1,12 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class DatabaseManager {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-    private static final Logger logger = LoggerFactory.getLogger(DatabaseManager.class);
+public class DatabaseManager {
 
     private static Connection getRemoteConnection() {
         if (System.getenv("RDS_HOSTNAME") != null) {
@@ -18,15 +18,38 @@ public class DatabaseManager {
                 String hostname = System.getenv("RDS_HOSTNAME");
                 String port = System.getenv("RDS_PORT");
                 String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-                logger.trace("Getting remote connection with connection string from environment variables.");
+                //logger.trace("Getting remote connection with connection string from environment variables.");
                 Connection con = DriverManager.getConnection(jdbcUrl);
-                logger.info("Remote connection successful.");
+                //logger.info("Remote connection successful.");
                 return con;
             }
-            catch (ClassNotFoundException e) { logger.warn(e.toString());}
-            catch (SQLException e) { logger.warn(e.toString());}
+            catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                //logger.warn(e.toString());
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+                //logger.warn(e.toString());
+            }
         }
         return null;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        try {
+            Connection con = getRemoteConnection();
+            Statement stmt = con.createStatement();
+            String sql = "CREATE TABLE ALLERGY " +
+                    "(userID varchar(15) not NULL, " +
+                    " restrictionID VARCHAR(15) not NULL, " +
+                    " allergyName VARCHAR(128), " +
+                    " sensitivity INTEGER, " +
+                    " PRIMARY KEY ( restrictionID ))";
+            stmt.execute(sql);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
     }
 
 
